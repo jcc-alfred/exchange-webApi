7d717135a44b034650d3888574d72d330a9f3898
alfred_jc <funky@23>
2018-10-22 19:08:58 +0800

    add kline limit

--- core/Routes/marketRouter.js
+++ core/Routes/marketRouter.js
@@ -9,6 +9,15 @@ router.post('/trade/kline',async(req,res,next)=>{
     if(klineList && klineList.length > 0){
       klineList = klineList.sort((item1,item2)=>{return item1.timestamp - item2.timestamp})
     }
+    let limit = 0;
+    let minArr = [60000, 300000, 900000, 1800000];//1,5 15 30
+    // let hourArr = [14400000, 21600000, 43200000,86400000];//4 6 12
+    if (minArr.includes(req.body.range)) {
+      limit = 300;
+    } else {
+      limit = 100;
+    }
+    klineList = klineList.slice(0, limit);
     klineList.forEach(item => {
       let tmpArray = [item.timestamp * 1000,item.open_price,item.high_price,item.low_price,item.close_price,item.volume];
       klineArray.push(tmpArray);
