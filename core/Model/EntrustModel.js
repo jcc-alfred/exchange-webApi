ffd0cc9f5c0a78ad9c2f5e8b50dc2f464e768ff0
Jie Xiao <jie.xiao@gtdollar.com>
2018-11-05 13:51:15 +0800

    add return for promise

--- core/Model/EntrustModel.js
+++ core/Model/EntrustModel.js
@@ -203,7 +203,7 @@ class EntrustModel {
           marketModel.last_price = marketRes.close_price;
         }
         marketList.push({coin_exchange_id: item.coin_exchange_id, market: marketModel, coinEx: item});
-
+        return true;
       }));
       try {
         let coin_prices = await axios.get(config.GTdollarAPI, {timeout: 2000});
