5c9e6586d914da173983d0fbfd5d1a3cba463901
Jie Xiao <jie.xiao@gtdollar.com>
2019-06-28 13:49:00 +0800

    update

--- core/Routes/marketRouter.js
+++ core/Routes/marketRouter.js
@@ -1,6 +1,8 @@
 let express = require('express');
 let router = express.Router();
 let EntrustModel = require('../Model/EntrustModel');
+let Enumerable = require('linq');
+
 
 router.post('/trade/kline',async(req,res,next)=>{
   try {
@@ -17,7 +19,7 @@ router.post('/trade/kline',async(req,res,next)=>{
     } else {
       limit = 100;
     }
-    klineList = klineList.slice(0, limit);
+    klineList = Enumerable.from(klineList).orderByDescending("parseFloat($.timestamp)").take(10).toArray();
     klineList.forEach(item => {
       let tmpArray = [item.timestamp * 1000,item.open_price,item.high_price,item.low_price,item.close_price,item.volume];
       klineArray.push(tmpArray);
