5e22849a2bc082df6ce0c5c1e507a8b81c21ed7c
Jie Xiao <jie.xiao@gtdollar.com>
2019-10-15 17:18:29 +0800

    update

--- core/Model/WithdrawModel.js
+++ core/Model/WithdrawModel.js
@@ -67,8 +67,9 @@ class WithdrawModel {
         confirmStatus = 1;
         confirmStatusName = "已审核"
       } else if (coinId === 5 && submitAmount <= 300) {
-        let AIM_Deposit_Count = await DepositModel.getUserDepositCountByCoinId(userId, 22);
-        if (AIM_Deposit_Count > 0) {
+        let GTH_Deposit = await DepositModel.getUserDepositCountByCoinId(userId, 20);
+        let GTC_Deposit = await DepositModel.getUserDepositCountByCoinId(userId, 201);
+        if (GTH_Deposit === 0 && GTC_Deposit === 0) {
           confirmStatus = 1;
           confirmStatusName = "已审核"
         }
