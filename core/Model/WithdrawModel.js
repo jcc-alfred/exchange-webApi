7b9c5a27361a78ceddf67123b90fe18bc8bd3c29
Jie Xiao <jie.xiao@gtdollar.com>
2019-11-05 11:41:25 +0800

    only aim deposit user can withdraw usdt

--- core/Model/WithdrawModel.js
+++ core/Model/WithdrawModel.js
@@ -67,9 +67,8 @@ class WithdrawModel {
         confirmStatus = 1;
         confirmStatusName = "已审核"
       } else if (coinId === 5 && submitAmount <= 300) {
-        let GTH_Deposit = await DepositModel.getUserDepositCountByCoinId(userId, 20);
-        let GTC_Deposit = await DepositModel.getUserDepositCountByCoinId(userId, 201);
-        if (GTH_Deposit === 0 && GTC_Deposit === 0) {
+        let AIM_Deposit = await DepositModel.getUserDepositCountByCoinId(userId, 22);
+        if (AIM_Deposit > 0) {
           confirmStatus = 1;
           confirmStatusName = "已审核"
         }
