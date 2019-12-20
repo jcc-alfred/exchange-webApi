7131df1acd78b56227aca313fc9458df2b5fa205
Jie Xiao <jie.xiao@gtdollar.com>
2019-12-20 16:52:18 +0800

    disable gtb auto withdraw audit

--- core/Model/WithdrawModel.js
+++ core/Model/WithdrawModel.js
@@ -60,10 +60,11 @@ class WithdrawModel {
       if ([17, 22].indexOf(coinId) >= 0 && submitAmount <= 20000) {
         confirmStatus = 1;
         confirmStatusName = "已审核"
-      } else if ([8].indexOf(coinId) >= 0) {
-        confirmStatus = 1;
-        confirmStatusName = "已审核"
       }
+      // else if ([8].indexOf(coinId) >= 0) {
+      //   confirmStatus = 1;
+      //   confirmStatusName = "已审核"
+      // }
       else if (coinId === 5 && submitAmount <= 300) {
         let AIM_Deposit = await DepositModel.getUserDepositCountByCoinId(userId, 22);
         if (AIM_Deposit > 0) {
